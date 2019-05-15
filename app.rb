097ae3825006a3410b9e497561b18b7a3dbb9ce2
Tracy Ngo <tngo1206@gmail.com>
2019-05-15 01:52:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-   "My name is _"
+   "My name is _."
   end
   
   get '/hometown' do
-    "My hometown is _"
+    "My hometown is _."
   end
   
   get '/favorite-song' do
-    "My favorite song is _"
+    "My favorite song is _."
   end
 end
