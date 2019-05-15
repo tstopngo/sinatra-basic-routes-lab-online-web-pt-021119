6265e2ca12d33b71a2dbc3f7d57fc3f2d0c7322b
Tracy Ngo <tngo1206@gmail.com>
2019-05-15 01:38:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
+   "My name is _"
+  end
+  
+   get '/hometown' do
   
   end
   
+   get '/favorite-song' do
+  
+  end
 end
